c198f06d6bad0b7f5ff03fad1cdb35831f790211
HazaelPS <saddayhazaelpinedasoledad@gmail.com>
2026-02-14 16:03:17 -0600

    Logro: Inicio Proyecto Programación Estructurada

---
+++ src/Clase27.java
@@ -0,0 +1,28 @@
+public class Clase27 {
+    static void main() {
+    /*
+    int i =1;
+    int n=50;
+    do{
+        if(n==3) {
+            break;
+        }
+        system.out.println(i);
+        i++;
+    }while (i<=n){
+        2<=50
+        3<50
+    }
+    int i=1;
+    int n=6;
+    while   (i<=n){
+        if(i/2=while=0){
+            continue;
+            i++;
+        }
+        system.out.println(i);
+    }
+
+     */
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(){
+    /*
+    int i =1;
+    int n=50;
+    do{
+        if(n==3) {
+            break;
+        }
+        system.out.println(i);
+        i++;
+    }while (i<=n){
+        2<=50
+        3<50
+    } */
+    int i=1;
+    int n=6;
+    while   (i<=n){
+        if(i/2==0){
+            continue;
+            i++;
+        }
+        system.out.println(i);
+    }
+}
